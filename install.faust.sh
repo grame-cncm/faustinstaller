c6affff53449c7674030dea9b1202d4c92c8790c
Yann Orlarey <orlarey@grame.fr>
2017-03-28 11:54:21 +0200

    added missing multilib support

--- install.faust.sh
+++ install.faust.sh
@@ -18,7 +18,7 @@ function installfaust {
 	$SUDO apt-get -y update
 	echo "Installing Faust dependencies..."
 	echo yes | $SUDO apt install -y jackd2
-	$SUDO apt-get install -y build-essential pkg-config git libmicrohttpd-dev llvm-3.6 llvm-3.6-dev libssl-dev ncurses-dev libsndfile-dev libedit-dev libcurl4-openssl-dev vim-common
+	$SUDO apt-get install -y build-essential g++-multilib pkg-config git libmicrohttpd-dev llvm-3.6 llvm-3.6-dev libssl-dev ncurses-dev libsndfile-dev libedit-dev libcurl4-openssl-dev vim-common
 
 	# Install all the needed SDK
 	$SUDO apt-get install -y libgtk2.0-dev libasound2-dev libqrencode-dev
