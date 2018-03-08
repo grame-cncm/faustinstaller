fbbf55addf83f71b34bafb406432eac65f0ed420
D. Fober <fober@grame.fr>
2018-03-08 14:19:44 +0100

    in progress...

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ log=install.log
 
 ####################################################
 # install faust
-sh install.faustservice.sh
-sh install.faust.sh
-sh install.faustwebsite.sh
-sh install.onlinecompiler.sh
-sh install.faustplayground.sh
-echo -n "Faust services installed on " >>$log
+sh ./install.faustservice.sh
+sh ./install.faust.sh
+sh ./install.faustwebsite.sh
+sh ./install.onlinecompiler.sh
+sh ./install.faustplayground.sh
+echo -n "Faust services installed on " >> $log
 date >> $log
