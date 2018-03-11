21f493bc3a41e400250e970d20877dbe0a364606
Yann Orlarey <orlarey@gmail.com>
2018-03-10 17:05:28 -0800

    Simplified installer
    Only installs the faust compiler itself, not the various SDKs

---
+++ install.compiler.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+####################################################
+# various settings are here
+####################################################
+FAUSTBRANCH=master-dev
+FAUSTDEPENDS="build-essential cmake"
+
+
+####################################################
+installfaust() {
+	# Install 'Installation directory' if needed
+	[ -d ~/FaustInstall ] || mkdir ~/FaustInstall
+	cd ~/FaustInstall
+
+	# for some reason which sudo doesn't work with Docker Ubuntu 16.04
+	#SUDO=`which sudo`
+	if [ -e /usr/bin/sudo ]; then
+		SUDO=/usr/bin/sudo
+	fi
+
+	
+	# Install Faust if needed
+	echo "###################### Install faust..."
+	[ -d "faust" ] || git clone https://github.com/grame-cncm/faust.git
+
+	# Update and compile Faust
+	cd faust
+	git checkout $FAUSTBRANCH
+	git pull
+	make
+	$SUDO make install  # can't use newinstall
+	faust -v
+	cd ..
+
+	echo "Installation Done!"
+}
+
+installfaust
