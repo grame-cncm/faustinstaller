d85d5900ad9d8ef3f7523546572e025efd3d91cc
Yann Orlarey <orlarey@gmail.com>
2019-01-07 11:11:56 +0100

    New install script for a virtual machine not running faustservice

---
+++ install.vm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+log=install.log
+
+####################################################
+# Specila installation for cloud based VM
+# install faust and associated services (but not faustservice)
+./install.compiler.sh
+echo
+./install.faustwebsite.sh
+echo
+./install.faustservice.sh
+echo
+./install.onlinecompiler.sh
+echo
+./install.faustplayground.sh
+echo
+./install.editor.sh
+echo "Faust services installation done"
+echo -n "Faust services installed on " >> $log
+echo
+./install.https.sh
+
+date >> $log
